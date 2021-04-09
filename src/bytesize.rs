ef9b31c665ccbb51fb4c3c10633c66854cc547c3
Miraclx <omiraculous@gmail.com>
2021-04-09 14:32:00 +0100

    group Format-s and add examples

--- src/bytesize.rs
+++ src/bytesize.rs
@@ -18,19 +18,27 @@ mod flags {
     bitflags! {
         #[derive(Default)]
         pub struct Format: u16 {
-            const Default                = 0 << 0;
-            const Initials               = 1 << 0;
-            const Condensed              = 1 << 1;
-            const Long                   = 1 << 2;
-            const NoPlural               = 1 << 3;
-            const ForcePlural            = 1 << 4;
-            const NoMultiCaps            = 1 << 5;
-            const LowerCaps              = 1 << 6;
-            const UpperCaps              = 1 << 7;
-            const NoFraction             = 1 << 8;
-            const ForceFraction          = 1 << 9;
-            const ShowThousandsSeparator = 1 << 10;
-            const NoSpace                = 1 << 11;
+            const Default                = 0 << 0; // 1 B, 2.13 KB, 1024.43 MiB
+
+            const Initials               = 1 << 0; // 1 B, 2.13 K, 1024.43 M
+            const Condensed              = 1 << 1; // 1 B, 2.13 KB, 1024.43 MB
+            const Long                   = 1 << 2; // 1 Byte, 2.13 KiloBytes, 1024.43 MebiBytes
+
+            // (requires Long)
+            const NoPlural               = 1 << 3; // 1 Byte, 2.13 KiloByte, 1024.43 MebiByte
+            const ForcePlural            = 1 << 4; // 1 Bytes, 2.13 KiloBytes, 1024.43 MebiBytes
+
+            // (requires Long)
+            const NoMultiCaps            = 1 << 5; // 1 Byte, 2.13 Kilobytes, 1024.43 Mebibytes
+
+            const LowerCaps              = 1 << 6; // 1 b, 2.13 kb, 1024.43 mib
+            const UpperCaps              = 1 << 7; // 1 B, 2.13 KB, 1024.43 MIB
+
+            const NoFraction             = 1 << 8; // 1 B, 2 KB, 1024 MiB
+            const ForceFraction          = 1 << 9; // 1.00 B, 2.13 KB, 1024.43 MiB
+
+            const ShowThousandsSeparator = 1 << 10; // 1 B, 2.13 KB, 1,024.43 MiB
+            const NoSpace                = 1 << 11; // 1B, 2.13KB, 1024.43MiB
         }
     }
 }
